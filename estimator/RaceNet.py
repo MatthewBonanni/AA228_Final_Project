6e9095cd83990cd17ccfd2d3f8941dabb37cac06
domyam97 <domyam97@gmail.com>
2023-11-16 16:16:58 -0800

    firt impl of BayesNet

--- estimator/RaceNet.py
+++ estimator/RaceNet.py
@@ -6,6 +6,8 @@ import torch
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader, random_split
 
+from bayesian_torch.models.dnn_to_bnn import dnn_to_bnn, get_kl_loss 
+
 from torch.utils.tensorboard import SummaryWriter
 
 import pandas as pd
@@ -16,12 +18,23 @@ from tqdm import tqdm
 
 Timedelta = pd._libs.tslibs.timedeltas.Timedelta
 args = {
-      'num_layers': 4,
-      'hidden_dim': 256,
+      'num_layers': 3,
+      'hidden_dim': 512,
       'out_dim': 1,
       'emb_dim': 10,
-      'dropout': 0.5,
+      'dropout': 0.4,
+      'num_monte_carlo': 100,
   }
+
+const_bnn_prior_parameters = {
+        "prior_mu": 0.0,
+        "prior_sigma": 1.0,
+        "posterior_mu_init": 0.0,
+        "posterior_rho_init": -3.0,
+        "type": "Reparameterization",  # Flipout or Reparameterization
+        "moped_enable": False,  # True to initialize mu/sigma from the pretrained dnn weights
+        "moped_delta": 0.5,
+}
 
 cols = [
     'Year',
@@ -58,14 +71,14 @@ class RaceNet(torch.nn.Module):
         self.activation = activation
         # Initialize Activation Fn
         self.batch_norms = torch.nn.ModuleList([torch.nn.BatchNorm1d(num_features=args["hidden_dim"])\
-                                                 for i in range(args["num_layers"]-1)])
+                                                 for i in range(args["num_layers"])])
 
         ## Initialize Linear Layers
         self.linears = \
             torch.nn.ModuleList(
                 [torch.nn.Linear(in_features=self.in_dim, out_features=args["hidden_dim"])])
         self.linears.extend([torch.nn.Linear(in_features=args["hidden_dim"], out_features=args["hidden_dim"])\
-                              for i in range(args["num_layers"])])
+                              for i in range(args["num_layers"]-1)])
         self.linears.append(torch.nn.Linear(in_features=args["hidden_dim"], out_features=args["out_dim"]))
 
         # Initialize Embeddings For Categorical Data
@@ -85,7 +98,7 @@ class RaceNet(torch.nn.Module):
             input = torch.cat((input, bed),dim=1)
         input = input.to(torch.float32)
 
-        for i in range(self.num_layers-1):
+        for i in range(self.num_layers):
             input = self.linears[i](input)      
             input = self.batch_norms[i](input)
             input = self.activation(input)
@@ -109,7 +122,7 @@ class F1Dataset(Dataset):
         val_cols = [i for i in cols if i not in ids]
         input_vals = data.loc[val_cols + weather_cols]
         for key, value in input_vals.items():
-            if type(value) == Timedelta:
+            if isinstance(value, Timedelta):
                 input_vals.loc[key] = value.total_seconds()
         input_vals = input_vals.to_numpy(dtype=float)
 
@@ -122,12 +135,14 @@ def train(dataloader, model, optimizer, loss_fn=F.mse_loss):
     model.train()
     loss = 0
 
-    for step, batch in enumerate(tqdm(dataloader, desc="Iteration")):
+    for batch in tqdm(dataloader, desc="Iteration"):
         optimizer.zero_grad()
-
-        out = model(batch[0],batch[1]).squeeze()
-        label = batch[2].squeeze().to(torch.float32)
-        loss = loss_fn(out, label)
+        with torch.autocast(device_type="cuda"):
+            out = model(batch[0],batch[1]).squeeze()
+            label = batch[2].squeeze().to(torch.float32)
+            kl = get_kl_loss(model)
+            loss = loss_fn(out, label) + kl/batch[0].size(0)
+            
 
         loss.backward()
         optimizer.step()
@@ -137,16 +152,18 @@ def train(dataloader, model, optimizer, loss_fn=F.mse_loss):
 def eval(dataloader, model, loss_fn=F.mse_loss):
 
     model.eval()
-    loss = 0
+    test_acc = 0
+    with torch.no_grad():
+        for batch in tqdm(dataloader, desc="Iteration"):
+            output_mc = []
+            for mc_run in range(args["num_monte_carlo"]):
+                outs = model(batch[0],batch[1])
+                output_mc.append(outs)
+            output = torch.stack(output_mc)  
+            y_pred = output.mean(dim=0)
+            test_acc += (torch.abs(y_pred.data - batch[2].squeeze())/batch[2].squeeze()).mean()/len(dataloader)
 
-    for step, batch in enumerate(tqdm(dataloader, desc="Iteration")):
-
-        out = model(batch[0],batch[1]).squeeze()
-        label = batch[2].squeeze().to(torch.float32)
-
-        loss += loss_fn(out, label)/torch.mean(label)/len(dataloader)
-
-    return loss.item()
+    return test_acc
 
 if __name__=="__main__":
     print(torch.cuda.is_available())
@@ -159,12 +176,13 @@ if __name__=="__main__":
     train_data = splits[0]
 
     train_dataloader = DataLoader(train_data, batch_size = 1000, shuffle=True)
-    val_dataloader = DataLoader(splits[1],batch_size=100, shuffle=False)
+    val_dataloader = DataLoader(splits[1],batch_size=1000, shuffle=False)
     test_dataloader = DataLoader(splits[2],batch_size=100, shuffle=False)
 
     model = RaceNet(args, num_drivers=26, num_tracks=27, num_teams=11)
+    dnn_to_bnn(model, const_bnn_prior_parameters)
 
-    epochs = 100
+    epochs = 60
     optimizer = torch.optim.Adam(model.parameters(),lr=0.001,)
 
     for i in range(epochs):
@@ -177,7 +195,7 @@ if __name__=="__main__":
         print("Loss:", train_loss)
 
     test_loss = eval(test_dataloader, model, loss_fn =F.l1_loss)
-    print(test_loss)
+    print("Final Test Loss:",test_loss)
 
     now = datetime.now()
     filename = 'outputs/racenet_' + now.strftime('%H_%M_%S') + '.pt'
